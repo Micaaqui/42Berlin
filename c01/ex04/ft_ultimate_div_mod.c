232d78ea897145206648b61fa54392f07380eb20
Micaaqui <micasantosaqui@gmail.com>
2025-11-18 15:17:48 +0100

    ex04

---
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,22 @@
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int div;
+	int mod;
+	
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int a, b;
+	a = 10;
+	b = 3;
+	ft_ultimate_div_mod(&a, &b);
+	printf(" div: %d \n mod: %d", a, b);
+	return (0);
+}
