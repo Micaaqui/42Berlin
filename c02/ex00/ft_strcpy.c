a50da8459dd8391abf71c395c8115436e036098a
Micaaqui <micasantosaqui@gmail.com>
2026-02-05 22:31:18 +0100

    c02/ex00

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *source;
+	char destination[50];
+
+	source = "Hey there";
+	ft_strcpy(destination, source);
+	printf("%s\n", destination);
+}
