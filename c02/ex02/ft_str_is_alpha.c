85c6dff2a78559c41f515da4ba7beb978a1ac726
Micaaqui <micasantosaqui@gmail.com>
2026-02-05 22:48:11 +0100

    c02/ex02

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,31 @@
+#include <string.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A'
+				&& str[i] <= 'Z'))
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[] = "abcde";
+
+	int	n = ft_str_is_alpha(str);
+	printf("str: %d\n", n);
+}
